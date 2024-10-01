a90180ed3513ea770210c264671a6c16a46cce7b
juiceman1014 <justintran1014@gmail.com>
2024-09-30 20:48:49 -0700

    added routing to home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import {Routes, Route} from 'react-router-dom';
+import HomePage from "./pages/HomePage/HomePage.jsx"
 
 function App() {
 
   return(
     <Routes>
-      <Route path = "/" element = {<Home />} />
+      <Route path = "/" element = {<HomePage />} />
     </Routes>
   )
 
