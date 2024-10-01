2556bf3d238dcc4d92e4bcdcb3e9fa58532979f7
juiceman1014 <justintran1014@gmail.com>
2024-09-30 20:46:54 -0700

    initial home page

---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,11 @@
+import './HomePage.css'
+
+const HomePage = () =>{
+    return(
+        <div className = "parent-container">
+            <div>Home Page</div>
+        </div>
+    )
+}
+
+export default HomePage
