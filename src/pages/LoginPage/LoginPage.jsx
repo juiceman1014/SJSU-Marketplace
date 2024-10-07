c96e12ec92d6f326a56df5f34a49044eede5ea81
juiceman1014 <justintran1014@gmail.com>
2024-10-06 21:37:13 -0700

    login page template

---
+++ src/pages/LoginPage/LoginPage.jsx
@@ -0,0 +1,11 @@
+import './LoginPage.css'
+
+const LoginPage = () =>{
+    return(
+        <div className = "login-parent-container">
+            <div>Login Page</div>
+        </div>
+    )
+}
+
+export default LoginPage
