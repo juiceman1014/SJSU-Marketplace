bb7ea2cef766a0227d948835df847c502b99f27b
juiceman1014 <justintran1014@gmail.com>
2024-10-06 21:32:40 -0700

    register page template

---
+++ src/pages/RegisterPage/RegisterPage.jsx
@@ -0,0 +1,11 @@
+import './RegisterPage.css'
+
+const RegisterPage = () =>{
+    return(
+        <div className = "parent-container">
+            <div>Register Page</div>
+        </div>
+    )
+}
+
+export default RegisterPage
