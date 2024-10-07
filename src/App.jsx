c283735fee2e63fd4e28ae971730964c001a7ef1
juiceman1014 <justintran1014@gmail.com>
2024-10-06 21:37:30 -0700

    added routing for register and login

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import {Routes, Route} from 'react-router-dom';
 import HomePage from "./pages/HomePage/HomePage.jsx"
+import RegisterPage from "./pages/RegisterPage/RegisterPage.jsx"
+import LoginPage from "./pages/LoginPage/LoginPage.jsx"
 
 function App() {
 
   return(
     <Routes>
       <Route path = "/" element = {<HomePage />} />
+      <Route path = "/register" element = {<RegisterPage />} />
+      <Route path = "/login" element = {<LoginPage />} />
     </Routes>
   )
 
