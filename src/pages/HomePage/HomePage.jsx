b6456423c7a1ddffc9345ac81a747fd0ae62c050
juiceman1014 <justintran1014@gmail.com>
2024-10-09 19:54:21 -0700

    made routings functional

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,11 +1,12 @@
 import './HomePage.css'
+import { Link } from 'react-router-dom'; 
 
 const HomePage = () =>{
     return(
         <div className = "home-parent-container">
             <div className = "links-div">
-                <p>Register</p>
-                <p>Sign In</p>
+                <p><Link to="/register">Register</Link></p>
+                <p><Link to="/login">Log In</Link></p>
                 <p>Post</p>
             </div>
         </div>
