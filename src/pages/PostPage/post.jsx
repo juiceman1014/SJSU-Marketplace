658185fc0eaffa0569128bfe0730d8a8c3f7b336
juiceman1014 <justintran1014@gmail.com>
2024-10-09 19:56:28 -0700

    initial commit

---
+++ src/pages/PostPage/post.jsx
@@ -0,0 +1,11 @@
+import './HomePage.css'
+
+const HomePage = () =>{
+    return(
+        <div className = "post-parent-container">
+            
+        </div>
+    )
+}
+
+export default HomePage
