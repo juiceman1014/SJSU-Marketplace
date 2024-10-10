fab8e56091125f0ebe5aa1ad216e8b9cb425cef3
juiceman1014 <justintran1014@gmail.com>
2024-10-09 20:11:28 -0700

    initial commit

---
+++ src/pages/ProfilePage/ProfilePage.jsx
@@ -0,0 +1,11 @@
+import './ProfilePage.css'
+
+const ProfilePage = () =>{
+    return(
+        <div className = "post-parent-container">
+            <div>Profile Page</div>
+        </div>
+    )
+}
+
+export default ProfilePage
