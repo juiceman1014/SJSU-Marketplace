2081835c6267957a7da15f86406fb240039eb144
juiceman1014 <justintran1014@gmail.com>
2024-10-21 21:01:11 -0700

    iniitalizing variables

--- src/pages/ListingPage/ListingPage.jsx
+++ src/pages/ListingPage/ListingPage.jsx
@@ -1,7 +1,25 @@
 import "./ListingPage.css";
 import Header from "../../components/Header/Header";
+import { auth, db, storage } from "../../configuration/firebase-config.js";
+import { ref as dbRef, push, get, child } from "firebase/database";
+import { ref as storageRef, uploadBytes, getDownloadURL } from "firebase/storage";
+import Modal from "react-modal";
 
 const ListingPage = () => {
+  const [listings, setListings] = useState([]);
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [title, setTitle] = useState("");
+  const [category,setCategory] = useState("");
+  const [condition, setCondition] = useState("");
+  const [description, setDescription] = useState("");
+  const [price, setPrice] = useState("");
+  const [image, setImage] = useState(null);
+  const [uploading, setUploading] = useState(false);
+
+
+
+
+
   return (
     <div>
       <Header />
