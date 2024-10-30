49f5f8cf2d5aeba744b34013a4560dbda8e42ea8
juiceman1014 <justintran1014@gmail.com>
2024-10-29 22:32:54 -0700

    imports added and initialized variables

--- src/pages/MessagePage/MessagePage.jsx
+++ src/pages/MessagePage/MessagePage.jsx
@@ -1,8 +1,22 @@
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { db, auth } from "../../configuration/firebase-config";
+import { ref, onValue, push, update } from "firebase/database";
 import "./MessagePage.css";
 
 const MessagePage = () => {
+    const { buyerID, sellerID, conversationID } = useParams();
+    const [conversations, setConversations] = useState([]);
+    const [ messages, setMessage ] = useState([]);
+    const [ newMessages, setNewMesage] = useState("");
+    const currentUserID = auth.currentUser.uid;
+
+    
     return(
-        <div>Message Page</div>
+        <div>
+            <div className = "message-container">Message Page</div>
+        </div>
+
     );
 };
 
