de147139825cde33eb4c9fc691f193c7139e3fb2
juiceman1014 <justintran1014@gmail.com>
2024-10-29 22:28:41 -0700

    added routing for messaging page

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import RegisterPage from "./pages/RegisterPage/RegisterPage.jsx"
 import LoginPage from "./pages/LoginPage/LoginPage.jsx"
 import ListingPage from "./pages/ListingPage/ListingPage.jsx"
 import ProfilePage from './pages/ProfilePage/ProfilePage.jsx';
+import MessagePage from "./pages/MessagePage/MessagePage.jsx"
 
 function App() {
 
@@ -14,6 +15,8 @@ function App() {
       <Route path = "/login" element = {<LoginPage />} />
       <Route path = "/listing" element = {<ListingPage />} />
       <Route path = "/profile" element = {<ProfilePage />} />
+      <Route path = "/message" element ={<MessagePage/>}/>
+      <Route path = "/message/:buyerID/:sellerID/:conversationID" element ={<MessagePage/>}/>
     </Routes>
   )
 
