5067efe43274342a551d93a55421e942e7c7a7fe
juiceman1014 <justintran1014@gmail.com>
2024-10-30 18:50:50 -0700

    useEffect for display user's conversations

--- src/pages/MessagePage/MessagePage.jsx
+++ src/pages/MessagePage/MessagePage.jsx
@@ -11,6 +11,16 @@ const MessagePage = () => {
     const [ newMessages, setNewMesage] = useState("");
     const currentUserID = auth.currentUser.uid;
 
+    useEffect(() => {
+        const userConversationsRef = ref(db, `users/${currentUserID}/conversations`);
+        const unsubscribe = onValue(userConversationsRef, (snapshot) => {
+            if(snapshot.exists()){
+                setConversations(Object.keys(snapshot.val()));
+            }
+        });
+
+        return() => unsubscribe();
+    }, [currentUserID]);
     
     return(
         <div>
