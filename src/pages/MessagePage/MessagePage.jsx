2ea0c2c9a052358132877614a3e9a32ba3fc7bda
juiceman1014 <justintran1014@gmail.com>
2024-10-31 14:14:08 -0700

    removed unnecessary header

--- src/pages/MessagePage/MessagePage.jsx
+++ src/pages/MessagePage/MessagePage.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { db, auth } from "../../configuration/firebase-config";
 import { ref, onValue, push, update, get} from "firebase/database";
