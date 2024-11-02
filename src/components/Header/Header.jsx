256311d8f0d7dafb15df0ea2c8c6f9c0752326f5
juiceman1014 <justintran1014@gmail.com>
2024-11-01 22:35:15 -0700

    added link to messages page

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -9,7 +9,7 @@ const Header = () =>{
             <p><Link to="/login">Log In</Link></p>
             <p><Link to="/listing">Listings</Link></p>
             <p><Link to="/profile">Profile</Link></p>
-            <p><Link to="/conversation">Conversations</Link></p>
+            <p><Link to="/message">Messages</Link></p>
         </div>
     )
 }
