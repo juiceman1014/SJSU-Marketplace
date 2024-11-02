78d71c46f4b2debf45020faf30b9323de0687541
juiceman1014 <justintran1014@gmail.com>
2024-11-01 22:13:13 -0700

    moved links from this page to header

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,16 +1,9 @@
 import './HomePage.css'
-import { Link } from 'react-router-dom'; 
 
 const HomePage = () =>{
     return(
         <div className = "home-container">
-            <div className = "links-div">
-                <p><Link to="/register">Register</Link></p>
-                <p><Link to="/login">Log In</Link></p>
-                <p><Link to="/listing">Listings</Link></p>
-                <p><Link to="/profile">Profile</Link></p>
-                {/* <p><Link to="/message">Messages</Link></p> */}
-            </div>
+            Home Page
         </div>
     )
 }
