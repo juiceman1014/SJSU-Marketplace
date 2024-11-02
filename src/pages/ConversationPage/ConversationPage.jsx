f10c4f1aafe721ea3df92ad2dfa638a1477e5cec
juiceman1014 <justintran1014@gmail.com>
2024-11-01 22:20:29 -0700

    initial commit

---
+++ src/pages/ConversationPage/ConversationPage.jsx
@@ -0,0 +1,15 @@
+import "./ConversationPage.css"
+import Header from '../../components/Header/Header'
+
+const ConversationPage = () => {
+    return(
+        <div>
+            <Header/>
+            <div className = "conversation-container">
+                Conversations
+            </div>
+        </div>
+    )
+}
+
+export default ConversationPage
