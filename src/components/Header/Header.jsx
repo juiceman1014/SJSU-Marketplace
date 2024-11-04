b0600b588877f5e80cbfb9d9148d02538fff2e8f
juiceman1014 <justintran1014@gmail.com>
2024-11-03 20:18:49 -0800

    register linked removed, link can be found in login page

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,7 +5,6 @@ const Header = () =>{
     return(
         <div className = "header-parent-container">
             <p><Link to="/">Home</Link></p>
-            <p><Link to="/register">Register</Link></p>
             <p><Link to="/login">Log In</Link></p>
             <p><Link to="/listing">Listings</Link></p>
             <p><Link to="/profile">Profile</Link></p>
